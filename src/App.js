9765b24912c2b90ab455822f3cc3592f053c89cb
teresa <teresajclark@gmail.com>
2020-09-17 14:32:39 +0100

    api changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import {getPlantsDistribution} from './lib/api'
+import { getPlantsDistribution } from './lib/api'
 
 import DropDownFilter from './components/DropDownFilter'
 
