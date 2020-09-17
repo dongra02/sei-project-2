e6846e3d5293e77b22186e8d24533c34ee7b8b48
dongra02 <dsgraham1@gmail.com>
2020-09-17 16:41:24 +0300

    api cors issues

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,18 +1,14 @@
 import axios from 'axios'
 
+// const corsFix = 'https://cors-anywhere.herokuapp.com'
+const corsFix = 'http://localhost:8080'
 const token = 'token=jvzv7evQ2knnSuZVSQxVv-kWuxdUbQgXKw-8hcMAJcY'
 const baseUrl = 'https://trefle.io/api/v1'
 
-const withHeaders = () => {
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  }
-}
+
 
 export const getPlantsDistribution = (region) => {
-  console.log(`${baseUrl}/distributions/${region}/plants?${token}`, withHeaders())
-  return axios.get(`${baseUrl}/distributions/${region}/plants?${token}`, withHeaders())
+  console.log(`${corsFix}/${baseUrl}/distributions/${region}/plants?${token}`)
+  return axios.get(`${corsFix}/${baseUrl}/distributions/${region}/plants?${token}`)
 }
 
